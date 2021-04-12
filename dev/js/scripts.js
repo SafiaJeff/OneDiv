04fa7df334490fedb5617022ec93561c507a46dc
peanvtman <sgj29@drexel.edu>
2021-04-12 09:05:34 -0400

    Square scared+shakes

--- dev/js/scripts.js
+++ dev/js/scripts.js
@@ -21,4 +21,24 @@ SquareTL
     .to("#circle", {duration: .05, scaleY: .5})
     .to("#circle", {duration: .05, scaleY: .75})
     .to("#circle", {y:20, duration: .1, scaleY:1, scaleX:1})
+
+    //Square scared
+    .to("#square", { x:"+5", y:"-100", duration:.2, scaleX:1.6, scaleY:1.6})
+    .to("#square", {x:"-5", duration:.1})
+    .to("#square", {x:"+5", y:"0", duration:.1})
+    .to("#square", {x:"-5", duration:.1})
+    .to("#square", {x:"+5", duration:.1})
+    .to("#square", {x:"-5", duration:.1})
+    .to("#square", {x:"+5", duration:.1})
+    .to("#square", {x:"-5", duration:.1})
+    .to("#square", {x:"+5", duration:.1})
+    .to("#square", {x:"-5", duration:.1, scaleX:1, scaleY:1})
+    .to("#square", {x:"-5", duration:.1})
+    .to("#square", {x:"+5", duration:.1})
+  
+    //Square approaches
+    
+
+
+
     ;
