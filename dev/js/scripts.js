94acf764cc5129055314fc97987663c02cb2f60c
peanvtman <sgj29@drexel.edu>
2021-04-12 08:46:08 -0400

    circle anim done

--- dev/js/scripts.js
+++ dev/js/scripts.js
@@ -11,14 +11,14 @@ SquareTL
     .to("#square", {duration: .05, y: -50, scaleY: .25})
     .to("#square", {duration: .05, scaleY: .5})
     .to("#square", {duration: .05, scaleY: .75})
-    .to("#square", {y:0, scaleY:1, scaleX:1})
+    .to("#square", {y:0,duration: .1, scaleY:1, scaleX:1})
     
-//     //Enters circle
+    //Enters circle
     .from("#circle", {duration: .2, y: -600, delay: 1, alpha: 1})
     .to("#circle", {duration: .05, scaleY: .75, scaleX: 1.2})
     .to("#circle", {duration: .05, scaleY: .5, scalex: 1})
     .to("#circle", {duration: .05, y: -50, scaleY: .25})
     .to("#circle", {duration: .05, scaleY: .5})
     .to("#circle", {duration: .05, scaleY: .75})
-    .to("#circle", {y:0, scaleY:1, scaleX:1})
+    .to("#circle", {y:20, duration: .1, scaleY:1, scaleX:1})
     ;
