1724e0bb1ab968f04406f9134c7071ce15436050
peanvtman <sgj29@drexel.edu>
2021-04-11 21:51:45 -0400

    Fixed grid

--- dev/js/scripts.js
+++ dev/js/scripts.js
@@ -1,23 +1,24 @@
-import { gsap } from "gsap";
+// import { gsap } from "gsap";
 
-let SquareTL = gsap.timeline();
+// let SquareTL = gsap.timeline();
 
-Squcircle
-    // Falling from sky
-    .from("#square", {duration: .2, y: -600, delay: 1})
-    .to("#square", {duration: .05, scaleY: .75, scaleX: 1.2})
-    .to("#square", {duration: .05, scaleY: .5, scalex: 1})
-    .to("#square", {duration: .05, y: -50, scaleY: .25})
-    .to("#square", {duration: .05, scaleY: .5})
-    .to("#square", {duration: .05, scaleY: .75})
-    .to("#square", {y:0, scaleY:1})
+// SquareTL
+
+//     // Falling from sky
+//     .from("#square", {duration: .2, y: -600, delay: 1})
+//     .to("#square", {duration: .05, scaleY: .75, scaleX: 1.2})
+//     .to("#square", {duration: .05, scaleY: .5, scalex: 1})
+//     .to("#square", {duration: .05, y: -50, scaleY: .25})
+//     .to("#square", {duration: .05, scaleY: .5})
+//     .to("#square", {duration: .05, scaleY: .75})
+//     .to("#square", {y:0, scaleY:1})
     
-    //Enters circle
-    .from("#circle", {duration: .2, y: -600, delay: 1})
-    .to("#circle", {duration: .05, scaleY: .75, scaleX: 1.2})
-    .to("#circle", {duration: .05, scaleY: .5, scalex: 1})
-    .to("#circle", {duration: .05, y: -50, scaleY: .25})
-    .to("#circle", {duration: .05, scaleY: .5})
-    .to("#circle", {duration: .05, scaleY: .75})
-    .to("#circle", {y:0, scaleY:1})
-    ;
+//     //Enters circle
+//     // .from("#circle", {duration: .2, y: -600, delay: 1, alpha: 1})
+//     // .to("#circle", {duration: .05, scaleY: .75, scaleX: 1.2})
+//     // .to("#circle", {duration: .05, scaleY: .5, scalex: 1})
+//     // .to("#circle", {duration: .05, y: -50, scaleY: .25})
+//     // .to("#circle", {duration: .05, scaleY: .5})
+//     // .to("#circle", {duration: .05, scaleY: .75})
+//     // .to("#circle", {y:0, scaleY:1})
+//     ;
