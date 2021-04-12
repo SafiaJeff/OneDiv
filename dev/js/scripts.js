9614bb5ce6bce857b85e590e4db83b364ccd796e
peanvtman <sgj29@drexel.edu>
2021-04-11 21:21:41 -0400

    Started anim

--- dev/js/scripts.js
+++ dev/js/scripts.js
@@ -1,3 +1,6 @@
 import { gsap } from "gsap";
 
-gsap.from("#square", { duration: 4, scale: 2, delay: 2, repeat: -1, yoyo: true });
+let SquareTL = gsap.timeline();
+
+SquareTL
+    .from("#square", {duration: .25, y: -600, delay: 2});
